53b1bcea7c504b5cc7fdc035e168a359a2bcbd80
scastrillone <santiagocastrillon.ep@gmail.com>
2023-11-09 15:11:51 -0500

    adjust

--- infra/cdk.out/asset.525ffe3b1fd8a4ac9666098e2494f40b48484c0fa7ef7bb7f688e2276a24ce68/response_identity_verifier.py
+++
@@ -1,5 +0,0 @@
-class IdentityVerifierResponse:
-    def __init__(self,similarity,reason,confidence):
-        self.similarity = similarity
-        self.reason=reason
-        self.confidence = confidence
--- infra/cdk.out/asset.d157156058636de16bad65ad781c2f2e2c02365c3b66c2326af5b3c1d8f5905d/values_objects.py
+++
@@ -1,6 +0,0 @@
-from errors import NoFaceDataException,NoFaceMatchException,FaceMatchCreationException
-
-class FaceImage:
-    def __init__(self, image_bytes, metadata=None):
-        self.image_bytes = image_bytes
-        self.metadata = metadata or {}
